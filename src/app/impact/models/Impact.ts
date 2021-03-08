90defce8ebf556eb910e1ddd693738665a31327c
Henning Buhl <henning.buhl@yahoo.de>
2021-03-08 19:23:30 +0100

    Added impact relations

--- src/app/impact/models/Impact.ts
+++ src/app/impact/models/Impact.ts
@@ -1,5 +1,12 @@
+import { Analysis } from './Analysis';
+import { Stakeholder } from './Stakeholder';
+import { Dimension } from "./Dimension";
+
 export class Impact {
 	id: string = '';
 	value: number = 0;
 	description: string = '';
+	dimension!: Dimension;
+	stakeholder!: Stakeholder;
+	analysis!: Analysis;
 }
