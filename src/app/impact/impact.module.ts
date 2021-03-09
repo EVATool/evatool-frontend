a36be20f3279359b1cacfd6d964d8f64bdfa1617
Henning Buhl <henning.buhl@yahoo.de>
2021-03-09 20:49:32 +0100

    Added dependencies

--- src/app/impact/impact.module.ts
+++ src/app/impact/impact.module.ts
@@ -1,14 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImpactMainComponent } from './impact-main/impact-main.component';
 import { ImpactTableComponent } from './components/impact-table/impact-table.component';
 import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [ImpactMainComponent, ImpactTableComponent],
   imports: [
     CommonModule,
-    MatTableModule
+    MatTableModule,
+    MatSelectModule,
+    FormsModule,
+    MatSliderModule,
+    HttpClientModule
   ],
   exports: [
     ImpactMainComponent
