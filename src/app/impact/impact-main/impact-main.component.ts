85dbcfed754ae87fed53b2fb3bca1c4a816d5243
Henning Buhl <henning.buhl@yahoo.de>
2021-03-12 14:22:32 +0100

    Made impact header sticky

--- src/app/impact/impact-main/impact-main.component.ts
+++ src/app/impact/impact-main/impact-main.component.ts
@@ -1,7 +1,5 @@
 import { ImpactDataService } from './../services/impact/impact-data.service';
 import { Component, OnInit, ViewChild, EventEmitter, Output } from '@angular/core';
-import { Impact } from '../models/Impact';
-import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-impact-main',
@@ -9,7 +7,6 @@ import { MatTable } from '@angular/material/table';
   styleUrls: ['./impact-main.component.css']
 })
 export class ImpactMainComponent implements OnInit {
-  @ViewChild(MatTable) table!: MatTable<any>;
 
   constructor(private impactDataService: ImpactDataService) {
 
