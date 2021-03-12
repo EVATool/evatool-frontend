f7e238e1919c50331e65d2102a76f2d56abd2be9
fobaidi <fobaidi@gmx.de>
2021-03-12 15:25:34 +0100

    First steps for Angular
    First Steps

--- src/app/analysis/analysis-main/analysis-main.component.spec.ts
+++ src/app/analysis/analysis-main/analysis-main.component.spec.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AnalysisMainComponent } from './analysis-main.component';
@@ -6,6 +7,7 @@ describe('AnalysisMainComponent', () => {
   let component: AnalysisMainComponent;
   let fixture: ComponentFixture<AnalysisMainComponent>;
 
+  // @ts-ignore
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ AnalysisMainComponent ]
