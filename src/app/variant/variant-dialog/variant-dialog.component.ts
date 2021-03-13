74e82862cdd9122b9b260d64051ccd2a173c637f
JannisLuechtefeld <jannis.luechtefeld@yahoo.de>
2021-03-13 16:46:59 +0100

    form group

--- src/app/variant/variant-dialog/variant-dialog.component.ts
+++ src/app/variant/variant-dialog/variant-dialog.component.ts
@@ -4,7 +4,6 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {MatTableDataSource} from '@angular/material/table';
 import {Variant} from '../models/Variant';
 import {VariantDataService} from '../services/variant-data.service';
-import {Impact} from '../../impact/models/Impact';
 
 @Component({
   selector: 'app-variant-dialog',
@@ -23,6 +22,10 @@ export class VariantDialogComponent implements OnInit {
     private dialogRef: MatDialogRef<VariantDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private variantDataService: VariantDataService) {
+    this.variantDataService.onCreateVariant.subscribe(variant => {
+      console.log(variant);
+      this.matDataSource = new MatTableDataSource<Variant>(this.variants);
+    });
     this.variants = this.variantDataService.getVariants();
     this.matDataSource = new MatTableDataSource<Variant>(this.variants);
   }
@@ -30,12 +33,12 @@ export class VariantDialogComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.form = this.formBuilder.group({
+    this.form = this.formBuilder.group({index: new FormGroup({
       id: new FormControl(null),
       title: new FormControl(null),
       description: new FormControl(null),
       editable: new FormControl(null)
-   });
+   })});
 
     this.variantDataService.onCreateVariant.subscribe(variant => {
       console.log(variant);
@@ -56,8 +59,8 @@ export class VariantDialogComponent implements OnInit {
   }
 
   save(variant: Variant): void{
-    variant.title = this.form.value.title;
-    variant.description = this.form.value.description;
+    //variant.title = this.index.value.title;
+    //variant.description = this.index.value.description;
 
     this.variantDataService.save(variant);
   }
