faceebccf69f5ba125bf2a4104c90cb408d888c7
fobaidi <fobaidi@gmx.de>
2021-03-14 13:50:16 +0100

    quick Fixes

--- src/app/analysis/analysis.module.ts
+++ src/app/analysis/analysis.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnalysisMainComponent } from './analysis-main/analysis-main.component';
 import {StakeholderDialogComponent} from './stakeholder-dialog/stakeholder-dialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [AnalysisMainComponent, StakeholderDialogComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatIconModule,
+    FormsModule,
+    MatSelectModule
   ],
   exports: [
     AnalysisMainComponent
