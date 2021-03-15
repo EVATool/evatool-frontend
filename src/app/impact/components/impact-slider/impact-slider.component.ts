9d030d008692fa604ac2fbdc2a88b9dd347d9217
Henning Buhl <henning.buhl@yahoo.de>
2021-03-15 14:54:18 +0100

    Added impact slider component

---
+++ src/app/impact/components/impact-slider/impact-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-impact-slider',
+  templateUrl: './impact-slider.component.html',
+  styleUrls: ['./impact-slider.component.css']
+})
+export class ImpactSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
