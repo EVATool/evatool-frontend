1e160fb8a903f770c33485792d6ed61e2a766429
fobaidi <fobaidi@gmx.de>
2021-03-15 14:47:40 +0100

    Stakeholder View

---
+++ src/app/stakeholder/stakeholder-main/stakeholder-main.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {StakeholderMainComponent} from './stakeholder-main.component';
+
+
+describe('StakeholderMainComponent', () => {
+  let component: StakeholderMainComponent;
+  let fixture: ComponentFixture<StakeholderMainComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StakeholderMainComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StakeholderMainComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
