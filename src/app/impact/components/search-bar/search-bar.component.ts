2dbdcc6b8a88ebdae552938cd193b07a694b870c
Henning Buhl <henning.buhl@yahoo.de>
2021-03-18 11:40:20 +0100

    Added text changed event

--- src/app/impact/components/search-bar/search-bar.component.ts
+++ src/app/impact/components/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  @Output() searchTextChanged = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  searchTextChange(event: any) {
+    //console.log(event.target.value);
+    this.searchTextChanged.emit(event.target.value);
+  }
 }
