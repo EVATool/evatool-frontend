8f950c959a7b6343e3874a48209a70342e5844be
Henning Buhl <henning.buhl@yahoo.de>
2021-03-18 10:08:03 +0100

    Added rest uris

--- src/app/impact/settings/RestSettings.ts
+++ src/app/impact/settings/RestSettings.ts
@@ -8,9 +8,12 @@ export class RestSettings {
     })
   };
 
-  // WRONG URLS!
-  public static readonly dimensionsUrl = '/api/dimensions';
-  public static readonly stakeholdersUrl = '/api/stakeholders';
-  public static readonly analysesUrl = '/api/analysis';
-  public static readonly impactsUrl = '/api/impacts';
+  private static readonly serverIp = '79.171.179.211';
+  private static readonly serverPort = '443';
+  private static readonly baseUrl = `http://${RestSettings.serverIp}:${RestSettings.serverPort}/dimensions`;
+
+  public static readonly dimensionsUrl = 'http://79.171.179.211:443/dimensions';
+  public static readonly stakeholdersUrl = 'http://79.171.179.211:443/stakeholders';
+  public static readonly analysesUrl = 'http://79.171.179.211:443/analysis';
+  public static readonly impactsUrl = 'http://79.171.179.211:443/impacts';
 }
