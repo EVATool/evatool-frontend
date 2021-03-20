4a29f74d17cd2e63384e20b36b5da7a03bfa6569
Henning Buhl <henning.buhl@yahoo.de>
2021-03-20 11:55:43 +0100

    Implemented rest calls for impact

--- src/app/impact/services/impact/impact-rest.service.ts
+++ src/app/impact/services/impact/impact-rest.service.ts
@@ -16,4 +16,16 @@ export class ImpactRestService {
   getImpacts(): Observable<ImpactDto[]> {
     return this.http.get<ImpactDto[]>(RestSettings.impactsUrl);
   }
+
+  createImpact(impactDto: ImpactDto): Observable<any> {
+    return this.http.post(RestSettings.impactsUrl, impactDto, RestSettings.httpOptions);
+  }
+
+  updateImpact(impactDto: ImpactDto): Observable<any> {
+    return this.http.put(RestSettings.impactsUrl, impactDto, RestSettings.httpOptions);
+  }
+
+  deleteImpact(impactDto: ImpactDto): Observable<any> {
+    return this.http.delete(RestSettings.impactsUrl + '/' + impactDto.id);
+  }
 }
