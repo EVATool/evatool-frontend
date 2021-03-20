507289889e4064a6ae4b4d8b841420706fcbb166
Henning Buhl <henning.buhl@yahoo.de>
2021-03-20 16:41:37 +0100

    refactored logger

--- src/app/impact/settings/LogService.service.ts
+++ src/app/impact/settings/LogService.service.ts
@@ -17,38 +17,42 @@ export class LogServiceService {
 
   public static readonly logLevel: LogLevel = LogLevel.Info;
 
+  public static shouldLog(logLevel: LogLevel): boolean {
+    return LogServiceService.logLevel <= logLevel;
+  }
+
   trace(msg: any) {
-    if (LogServiceService.logLevel <= LogLevel.Trace) {
+    if (LogServiceService.shouldLog(LogLevel.Trace)) {
       console.log(msg);
     }
   }
 
   debug(msg: any) {
-    if (LogServiceService.logLevel <= LogLevel.Debug) {
+    if (LogServiceService.shouldLog(LogLevel.Debug)) {
       console.log(msg);
     }
   }
 
   info(msg: any) {
-    if (LogServiceService.logLevel <= LogLevel.Info) {
+    if (LogServiceService.shouldLog(LogLevel.Info)) {
       console.log(msg);
     }
   }
 
   warn(msg: any) {
-    if (LogServiceService.logLevel <= LogLevel.Warn) {
+    if (LogServiceService.shouldLog(LogLevel.Warn)) {
       console.log(msg);
     }
   }
 
   error(msg: any) {
-    if (LogServiceService.logLevel <= LogLevel.Error) {
+    if (LogServiceService.shouldLog(LogLevel.Error)) {
       console.log(msg);
     }
   }
 
   fatal(msg: any) {
-    if (LogServiceService.logLevel <= LogLevel.Fatal) {
+    if (LogServiceService.shouldLog(LogLevel.Fatal)) {
       console.log(msg);
     }
   }
