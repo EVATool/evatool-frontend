b6172a0753c29edef4e4a12ebd3b3bb897362359
JannisLuechtefeld <jannis.luechtefeld@yahoo.de>
2021-03-22 13:34:43 +0100

    add search bar

--- src/app/stakeholder/components/stakeholder-table/stakeholder-table.component.ts
+++ src/app/stakeholder/components/stakeholder-table/stakeholder-table.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stakeholder-table',
   templateUrl: './stakeholder-table.component.html',
-  styleUrls: ['./stakeholder-table.component.sass']
+  styleUrls: ['./stakeholder-table.component.scss']
 })
 export class StakeholderTableComponent implements OnInit {
 
