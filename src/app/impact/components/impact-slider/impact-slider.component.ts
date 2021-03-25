9c21d643facb05fa696f6676eb11cb8a1d10f4c8
Henning Buhl <henning.buhl@yahoo.de>
2021-03-25 10:06:23 +0100

    Added filtering and locking to slider

--- src/app/impact/components/impact-slider/impact-slider.component.ts
+++ src/app/impact/components/impact-slider/impact-slider.component.ts
@@ -1,5 +1,5 @@
-import { LogService } from '../../settings/log.service';
-import { Stakeholder } from './../../models/Stakeholder';
+import { LogService } from './../../settings/log.service';
+import { SliderFilterSettings, SliderFilterType, SliderFilterBoundary } from './../column-slider-filter/SliderFilterSettings';
 import { MatSlider, MatSliderChange } from '@angular/material/slider';
 import { Component, Input, OnInit, ViewChild, AfterViewInit, Output, EventEmitter, ElementRef } from '@angular/core';
 
@@ -9,17 +9,27 @@ import { Component, Input, OnInit, ViewChild, AfterViewInit, Output, EventEmitte
   styleUrls: ['./impact-slider.component.css']
 })
 export class ImpactSliderComponent implements OnInit, AfterViewInit {
-  @Input() value!: number;
+  @Input() value: number = 0;
+  @Input() valueSecond: number = -1;
   @Input() deadzone: number = 0.0;
+  @Input() isFilter: boolean = false;
+
+  @Input() isRisk: boolean = false;
+  @Input() isGoal: boolean = false;
+
   @Output() sliderValueChange = new EventEmitter<MatSliderChange>();
   @ViewChild(MatSlider) slider!: MatSlider;
   @ViewChild('goal') goalBar!: ElementRef;
   @ViewChild('risk') riskBar!: ElementRef;
   @ViewChild('thumb') thumb!: ElementRef;
+  @ViewChild('thumbSecond') thumbSecond!: ElementRef;
 
   legalValue!: number;
+  sliderFilterSettings: SliderFilterSettings;
 
-  constructor(private logger: LogService) { }
+  constructor(private logger: LogService) {
+    this.sliderFilterSettings = SliderFilterSettings.getDefault();
+  }
 
   ngOnInit(): void {
 
@@ -30,12 +40,25 @@ export class ImpactSliderComponent implements OnInit, AfterViewInit {
     this.drawSlider();
   }
 
+  onInit(): void {
+
+  }
+
+  invalidate(): void {
+    this.logger.info(this, 'Invalidating');
+    this.drawSlider();
+  }
+
   sliderValueChanged(event: MatSliderChange): void {
     if (event.value !== null) {
       if (-this.deadzone < event.value && event.value < this.deadzone && event.value !== 0) {
-        this.logger.info('Slider Deadzone Around Zero');
+        this.logger.info(this, 'Slider Deadzone Around Zero');
+      } else if (this.isRisk && event.value > 0) {
+        this.logger.info(this, 'Risk Slider Cropping Value');
+      } else if (this.isGoal && event.value < 0) {
+        this.logger.info(this, 'Goal Slider Cropping Value');
       } else {
-        this.logger.info(`Slider Value Changed: ${event.value}`);
+        this.logger.info(this, `Slider Value Changed: ${event.value}`);
         this.value = event.value;
         this.legalValue = event.value;
         this.drawSlider();
@@ -43,17 +66,179 @@ export class ImpactSliderComponent implements OnInit, AfterViewInit {
     }
   }
 
+  sliderSecondValueChanged(event: MatSliderChange): void {
+    if (event.value !== null) {
+      if (-this.deadzone < event.value && event.value < this.deadzone && event.value !== 0) {
+        this.logger.info(this, 'Slider Deadzone Around Zero');
+      } else {
+        this.logger.info(this, `Slider Value Changed: ${event.value}`);
+        this.valueSecond = event.value;
+        this.drawSlider();
+      }
+    }
+  }
+
   sliderValueChangedFinal(event: MatSliderChange): void {
+    this.logger.info(this, `Slider Value Changed Final: ${this.legalValue}`);
     this.slider.value = this.legalValue;
     event.value = this.legalValue;
     this.sliderValueChange.emit(event);
   }
 
   drawSlider(): void {
-    this.riskBar.nativeElement.style.width = Math.max(-this.value * 50, 0) + '%';
-    this.goalBar.nativeElement.style.width = Math.max(this.value * 50, 0) + '%';
+    this.logger.info(this, 'Draw Slider');
+
+    // Default bar settings.
+    this.goalBar.nativeElement.style.display = 'block';
+    this.goalBar.nativeElement.style.position = 'absolute';
+    this.goalBar.nativeElement.style.width = '50%';
+    this.goalBar.nativeElement.style.height = '20px';
+    this.goalBar.nativeElement.style.marginLeft = '50%';
+
+    this.riskBar.nativeElement.style.width = '50%';
+    this.riskBar.nativeElement.style.height = '20px';
+    this.riskBar.nativeElement.style.float = 'right';
+    this.riskBar.nativeElement.style.marginRight = '50%';
+
+    // Default thumb settings.
+    const thumbs = [this.thumb, this.thumbSecond];
+    thumbs.forEach((thumb: any) => {
+      thumb.nativeElement.style.position = 'absolute';
+      thumb.nativeElement.style.backgroundColor = 'gray';
+      thumb.nativeElement.style.height = '20px';
+      thumb.nativeElement.style.width = '11px';
+      thumb.nativeElement.style.float = 'left';
+      thumb.nativeElement.style.opacity = '1';
+    });
+
+    if (this.isFilter) {
+      switch (this.sliderFilterSettings.sliderFilterType) {
+        case SliderFilterType.GreaterThan:
+          this.riskBar.nativeElement.style.width = '0%';
+
+          this.goalBar.nativeElement.style.backgroundColor = 'orange';
+          this.goalBar.nativeElement.style.float = 'right';
+          this.goalBar.nativeElement.style.position = 'relative';
+          this.goalBar.nativeElement.style.width = (50 - this.value * 50) + '%';
+
+          this.drawThumb(this.value, this.thumb);
+          break;
+
+        case SliderFilterType.LessThan:
+          this.goalBar.nativeElement.style.width = '0%';
+
+          this.riskBar.nativeElement.style.backgroundColor = 'orange';
+          this.riskBar.nativeElement.style.float = 'left';
+          this.riskBar.nativeElement.style.position = 'relative';
+          this.riskBar.nativeElement.style.width = (50 + this.value * 50) + '%';
+
+          this.drawThumb(this.value, this.thumb);
+          break;
+
+        case SliderFilterType.Equality:
+          this.goalBar.nativeElement.style.width = '0%';
+          this.riskBar.nativeElement.style.width = '0%';
+
+          this.drawThumb(this.value, this.thumb);
+          break;
+
+        case SliderFilterType.Bewtween:
+          const smallerValue = Math.min(this.value, this.valueSecond);
+          const biggerValue = Math.max(this.value, this.valueSecond);
+          this.riskBar.nativeElement.style.width = '0%';
+
+          if (smallerValue != biggerValue) {
+            this.goalBar.nativeElement.style.backgroundColor = 'orange';
+            this.goalBar.nativeElement.style.marginLeft = (50 + smallerValue * 50) + '%';
+            this.goalBar.nativeElement.style.width = ((biggerValue - smallerValue) * 50) + '%';
+          } else {
+            this.goalBar.nativeElement.style.width = '0%';
+          }
+
+          this.drawThumb(this.value, this.thumb);
+          this.drawThumb(this.valueSecond, this.thumbSecond);
+          break;
+
+        case SliderFilterType.Off:
+          this.goalBar.nativeElement.style.width = '0%';
+          this.riskBar.nativeElement.style.width = '0%';
+
+          this.drawThumb(this.value, this.thumb);
+          break;
+
+        default:
+          this.logger.warn(this, 'Unhandled Filter Type');
+          break;
+      }
+
+      switch (this.sliderFilterSettings.sliderFilterBoundary) {
+        case SliderFilterBoundary.Include:
+          thumbs.forEach((thumb: any) => {
+            thumb.nativeElement.style.backgroundColor = 'green';
+          });
+          break;
+
+        case SliderFilterBoundary.Exclude:
+
+          thumbs.forEach((thumb: any) => {
+            thumb.nativeElement.style.backgroundColor = 'red';
+          });
+          break;
+
+        default:
+          this.logger.warn(this, 'Unhandled Filter Boundary Type');
+          break;
+      }
+    } else {
+      if (this.isRisk) {
+        this.riskBar.nativeElement.style.float = 'right';
+        this.riskBar.nativeElement.style.position = 'relative';
+
+        this.goalBar.nativeElement.style.float = 'left';
+        this.goalBar.nativeElement.style.position = 'absolute';
+
+        this.riskBar.nativeElement.style.backgroundColor = 'red';
+        this.goalBar.nativeElement.style.backgroundColor = 'black';
+
+        this.riskBar.nativeElement.style.width = Math.max(-this.value * 50, 0) + '%';
+        this.goalBar.nativeElement.style.width = '50%';
+
+        this.drawThumb(this.value, this.thumb);
+      } else if (this.isGoal) {
+        this.riskBar.nativeElement.style.float = 'right';
+        this.riskBar.nativeElement.style.position = 'relative';
+
+        this.goalBar.nativeElement.style.float = 'left';
+        this.goalBar.nativeElement.style.position = 'absolute';
+
+        this.riskBar.nativeElement.style.backgroundColor = 'black';
+        this.goalBar.nativeElement.style.backgroundColor = 'green';
+
+        this.riskBar.nativeElement.style.width = '50%';
+        this.goalBar.nativeElement.style.width = Math.max(this.value * 50, 0) + '%';
+
+        this.drawThumb(this.value, this.thumb);
+      } else {
+        this.riskBar.nativeElement.style.float = 'right';
+        this.riskBar.nativeElement.style.position = 'relative';
+
+        this.goalBar.nativeElement.style.float = 'left';
+        this.goalBar.nativeElement.style.position = 'absolute';
+
+        this.riskBar.nativeElement.style.backgroundColor = 'red';
+        this.goalBar.nativeElement.style.backgroundColor = 'green';
+
+        this.riskBar.nativeElement.style.width = Math.max(-this.value * 50, 0) + '%';
+        this.goalBar.nativeElement.style.width = Math.max(this.value * 50, 0) + '%';
+
+        this.drawThumb(this.value, this.thumb);
+      }
+    }
+  }
+
+  drawThumb(value: number, thumb: any) {
     const map = (mapValue: number, x1: number, y1: number, x2: number, y2: number) => (mapValue - x1) * (y2 - x2) / (y1 - x1) + x2;
-    const val = map(this.value, -1, 1, 3, 97);
-    this.thumb.nativeElement.style.left = 'calc(' + val + '% - 5px)';
+    const val = map(value, -1, 1, 1, 99);
+    thumb.nativeElement.style.left = 'calc(' + val + '% - 5px)';
   }
 }
