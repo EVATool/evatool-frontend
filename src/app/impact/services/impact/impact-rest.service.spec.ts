3731f70f9b3c9788de1065a00b491267f25e9e80
Henning Buhl <henning.buhl@yahoo.de>
2021-03-25 13:02:53 +0100

    Added ImpactRestService tests

--- src/app/impact/services/impact/impact-rest.service.spec.ts
+++ src/app/impact/services/impact/impact-rest.service.spec.ts
@@ -47,7 +47,7 @@ describe('ImpactRestService', () => {
     it('should return an Observable<ImpactDto[]>', () => {
       // Arrage
       const analysisId = DataLoader.dummyImpactDtos[0].analysis.id
-      const dummyDtos = DataLoader.dummyImpactDtos.filter(impact => impact.analysis.id == analysisId);;
+      const dummyDtos = DataLoader.dummyImpactDtos.filter(impact => impact.analysis.id == analysisId);
 
       // Act
       service.getImpactsByAnalysisId(analysisId).subscribe(impacts => {
@@ -59,6 +59,57 @@ describe('ImpactRestService', () => {
       const req = httpMock.expectOne(RestSettings.impactsUrl + '?analysisId=' + analysisId);
       expect(req.request.method).toBe('GET');
       req.flush(dummyDtos);
+    });
+  });
+
+  describe('#createImpact', () => {
+    it('should return an Observable<any>', () => {
+      // Arrage
+      const dummyDto = DataLoader.dummyImpactDtos[0];
+
+      // Act
+      service.createImpact(dummyDto).subscribe(impact => {
+        expect(impact).toEqual(dummyDto);
+      });
+
+      // Assert
+      const req = httpMock.expectOne(RestSettings.impactsUrl);
+      expect(req.request.method).toBe('POST');
+      req.flush(dummyDto);
+    });
+  });
+
+  describe('#updateImpact', () => {
+    it('should return an Observable<any>', () => {
+      // Arrage
+      const dummyDto = DataLoader.dummyImpactDtos[0];
+
+      // Act
+      service.updateImpact(dummyDto).subscribe(impact => {
+        expect(impact).toEqual(dummyDto);
+      });
+
+      // Assert
+      const req = httpMock.expectOne(RestSettings.impactsUrl);
+      expect(req.request.method).toBe('PUT');
+      req.flush(dummyDto);
+    });
+  });
+
+  describe('#deleteImpact', () => {
+    it('should return an Observable<any>', () => {
+      // Arrage
+      const dummyDto = DataLoader.dummyImpactDtos[0];
+
+      // Act
+      service.deleteImpact(dummyDto).subscribe(impact => {
+        expect(impact).toEqual(dummyDto);
+      });
+
+      // Assert
+      const req = httpMock.expectOne(RestSettings.impactsUrl + '/' + dummyDto.id);
+      expect(req.request.method).toBe('DELETE');
+      req.flush(dummyDto);
     });
   });
 });
