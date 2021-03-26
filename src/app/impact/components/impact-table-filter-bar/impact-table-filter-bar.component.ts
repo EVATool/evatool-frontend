8500e5adec37b0529c4a2a14b8a0c8855a256af9
pschm <philipp.schmeier@gmail.com>
2021-03-26 12:18:56 +0100

    refactor sass to scss

--- src/app/impact/components/impact-table-filter-bar/impact-table-filter-bar.component.ts
+++ src/app/impact/components/impact-table-filter-bar/impact-table-filter-bar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-impact-table-filter-bar',
   templateUrl: './impact-table-filter-bar.component.html',
-  styleUrls: ['./impact-table-filter-bar.component.sass']
+  styleUrls: ['./impact-table-filter-bar.component.scss']
 })
 export class ImpactTableFilterBarComponent implements OnInit {
 
@@ -12,4 +12,6 @@ export class ImpactTableFilterBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  searchTextChange($event: string): void {
+  }
 }
