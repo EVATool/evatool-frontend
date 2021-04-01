5c441b50040a0b4e36a21161007777be3ea9749c
Henning Buhl <henning.buhl@yahoo.de>
2021-04-01 09:03:05 +0200

    Fixed scrollbar appearing on bottom of page

--- src/app/impact/components/impact-table/impact-table.component.spec.ts
+++ src/app/impact/components/impact-table/impact-table.component.spec.ts
@@ -1,4 +1,4 @@
-import { HighlightSearch } from './../../pipes/HighlightSearch';
+import { HighlightSearch } from '../../pipes/HighlightSearch';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormBuilder } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
