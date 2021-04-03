321c06f6a241206bebcf0d1247c5ea6c9fa8e640
Henning Buhl <henning.buhl@yahoo.de>
2021-04-03 14:58:29 +0200

    Adjusted StakeholderDto and Entity to new backend

--- src/app/impact/dtos/ImpactStakeholderDto.ts
+++ src/app/impact/dtos/ImpactStakeholderDto.ts
@@ -1,4 +1,5 @@
 export class ImpactStakeholderDto {
   id = '';
   name = '';
+  level = '';
 }
--- src/app/impact/dtos/StakeholderDto.ts
+++ src/app/impact/dtos/StakeholderDto.ts
@@ -1,4 +1,5 @@
 export class StakeholderDto {
   rootEntityID = '';
   stakeholderName = '';
+  stakeholderLevel = '';
 }
