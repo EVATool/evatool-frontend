6ba0bc5fc62571855be1b6c79de413941f95b1f2
Henning Buhl <henning.buhl@yahoo.de>
2021-04-04 23:35:51 +0200

    Added see references impacts of value option to value table

--- src/app/impact/components/impact-table/components/value-dialog/components/value-table/value-table.component.ts
+++ src/app/impact/components/impact-table/components/value-dialog/components/value-table/value-table.component.ts
@@ -2,7 +2,10 @@ import {ValueDataService} from '../../../../../../services/value/value-data.serv
 import {Value} from '../../../../../../models/Value';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
-import {Component, Input, OnInit, AfterViewInit, ViewChild} from '@angular/core';
+import {Component, Input, OnInit, AfterViewInit, ViewChild, Output, EventEmitter} from '@angular/core';
+import {ImpactDataService} from "../../../../../../services/impact/impact-data.service";
+import {LogService} from "../../../../../../../shared/services/log.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-value-table',
@@ -12,11 +15,16 @@ import {Component, Input, OnInit, AfterViewInit, ViewChild} from '@angular/core'
 export class ValueTableComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSort) sort: MatSort = new MatSort();
   @Input() type!: string;
+  @Output() userWantsToSeeReferencedImpacts: EventEmitter<Value> = new EventEmitter();
 
   tableDataSource: MatTableDataSource<Value> = new MatTableDataSource<Value>();
   displayedColumns = ['disable', 'name', 'description'];
 
-  constructor(private valueDataService: ValueDataService) {
+  constructor(
+    private logger: LogService,
+    private valueDataService: ValueDataService,
+    private impactDataService: ImpactDataService,
+    private snackBar: MatSnackBar) {
   }
 
   ngOnInit(): void {
@@ -43,7 +51,25 @@ export class ValueTableComponent implements OnInit, AfterViewInit {
     };
   }
 
-  toggleValueDisable(value: Value) {
-    value.disable = !value.disable;
+  toggleValueDisable(event: Event, value: Value) {
+    let numImpactsUseValue = 0;
+    this.impactDataService.impacts.forEach(impact => {
+      if (impact.valueEntity === value) {
+        numImpactsUseValue++;
+      }
+    });
+    if (numImpactsUseValue > 0) {
+      this.logger.warn(this, 'This value is still being used by ' + numImpactsUseValue + ' impacts');
+      const message = 'This value cannot be excluded from the available selection. It is still being used by ' + numImpactsUseValue + ' impacts.';
+      const action = 'show me'
+      const snackBarRef = this.snackBar.open(message, action, {duration: 5000});
+      snackBarRef.onAction().subscribe(() => {
+        // TODO highlight in table.
+        this.logger.info(this, 'User wants to see the impacts referencing the value');
+        this.userWantsToSeeReferencedImpacts.emit(value);
+      });
+    } else {
+      value.disable = !value.disable;
+    }
   }
 }
