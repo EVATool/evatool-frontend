f9f4e962698d9061353bd659182a2b271bdbc80b
Mertinator <47606292+Mertinator@users.noreply.github.com>
2021-04-05 00:57:21 +0200

    Update filter bar

--- src/app/requirement/components/requirement-table-filter-bar/RequirementTableFilterEvent.ts
+++ src/app/requirement/components/requirement-table-filter-bar/RequirementTableFilterEvent.ts
@@ -1,6 +1,8 @@
-// import { SliderFilterSettings } from '../../../shared/components/impact-slider/SliderFilterSettings';
-// export class RequirementTableFilterEvent {
-//   variantsFilter: string[];
-//   constructor() {
-//   }
-// }
+import { SliderFilterSettings } from '../../../shared/components/impact-slider/SliderFilterSettings';
+export class RequirementTableFilterEvent {
+  variantsFilter!: string[];
+  valueSystemFilter!: string[];
+  valueFilter!: SliderFilterSettings;
+  constructor() {
+  }
+}
