fac6adb1996ad1b16876f7d2618234fb3d2e793c
Mertinator <47606292+Mertinator@users.noreply.github.com>
2021-04-24 05:24:14 +0200

    Added more value tests

--- src/app/requirement/services/value/value-data.service.spec.ts
+++ src/app/requirement/services/value/value-data.service.spec.ts
@@ -2,18 +2,51 @@ import {TestBed} from '@angular/core/testing';
 import {HttpClientModule} from '@angular/common/http';
 
 import {ValueDataService} from './value-data.service';
+import {RestMock} from '../../../impact/spec/RestMock';
+import {SampleDataService} from '../spec/sample-data-service';
 
 describe('DimensionDataService', () => {
   let service: ValueDataService;
+  let data: SampleDataService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientModule, RestMock.imports],
+      providers: [RestMock.providers]
     });
+    data = TestBed.inject(SampleDataService)
     service = TestBed.inject(ValueDataService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should load values', () => {
+    service.onInit();
+    expect(service.values).toEqual(data.dummyValueDtos);
+  });
+
+  it('should create a value', () => {
+    service.onInit();
+    const existingValues = service.values.length;
+    service.createValue(data.dummyValueDtos[0]);
+    expect(service.values.length).toBe(existingValues + 1);
+  });
+
+  it('should update a value', () => {
+    service.onInit();
+    const updateValue = service.values[0];
+    updateValue.valueTitle = 'New title';
+    service.updateValue(updateValue);
+    expect(service.values).toContain(updateValue);
+  });
+
+  it('should delete Value', () => {
+    service.onInit();
+    const exisitingValues = service.values.length;
+    const deleteValue = service.values[0];
+    service.deleteValue(deleteValue);
+    expect(service.values.length).toBe(exisitingValues - 1);
   });
 });
