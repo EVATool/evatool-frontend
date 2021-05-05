46d005fb97cc532a530e28ae76b67b497acc39eb
Henning Buhl <henning.buhl@yahoo.de>
2021-05-05 13:48:05 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  backendUrl: "${HOST_URL}",
-  backendPort: "${BACKEND_PORT}"
+//  backendUrl: `${HOST_URL}`,
+  backendUrl: '${JAVA_HOME}',
+  backendPort: '${BACKEND_PORT}'
 };
