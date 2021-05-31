381f990e2a1881285d58ef854cbe5e1526b7cfc7
Henning Buhl <henning.buhl@yahoo.de>
2021-05-31 20:11:58 +0200

    Update stakeholders-table.component.ts

--- src/app/components/analysis-home/stakeholders/stakeholders-table/stakeholders-table.component.ts
+++ src/app/components/analysis-home/stakeholders/stakeholders-table/stakeholders-table.component.ts
@@ -85,11 +85,9 @@ export class StakeholdersTableComponent implements OnInit, AfterViewInit {
 
       const levelFilter = searchTerms.level.length === 0 || searchTerms.level.indexOf(data.level) !== -1;
 
-      console.log(searchTerms.priority)
       const priorityFilter = searchTerms.priority.length === 0 || searchTerms.priority.indexOf(data.priority) !== -1;
 
-      console.log(searchTerms.impacted)
-      const impactedFilter = searchTerms.impacted == null || (data.impacted != null && data.impacted <= searchTerms.impacted);
+      const impactedFilter = searchTerms.impacted == null || (data.impacted != null && data.impacted <= -searchTerms.impacted);
 
       return levelFilter && priorityFilter && impactedFilter;
     };
