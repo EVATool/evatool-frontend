36676adf3f4dd80ed3fb69f5d6649fdd38c63400
Henning Buhl <henning.buhl@yahoo.de>
2021-06-01 13:44:32 +0200

    Update log.service.ts

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -3,6 +3,8 @@ import {environment} from '../../environments/environment';
 
 /*
 Features:
+Purge script
+Sample Data
 Only filter selected/active impacts/deltas
 Highlight (mark-textarea) TODO text not instantly highlighted when typing (and wrongly updated) solution: manually set selection?, placeholder disappears when typing not already when focussing, placeholder color
 Visuals of ValueModal changes when there is no entry in impact table. Mat form required to load??
