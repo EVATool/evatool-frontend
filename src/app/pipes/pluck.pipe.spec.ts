13816beb783b136f611154e9eb57ff94637576ad
Henning Buhl <henning.buhl@yahoo.de>
2021-06-07 14:41:41 +0200

    Added PluckPipe test

--- src/app/pipes/pluck.pipe.spec.ts
+++ src/app/pipes/pluck.pipe.spec.ts
@@ -1,10 +1,21 @@
 import {HighlightSearchPipe} from './highlight-search.pipe';
+import {PluckPipe} from './pluck.pipe';
 
 describe('PluckPipe', () => {
   it('create an instance', () => {
-    const pipe = new HighlightSearchPipe();
+    const pipe = new PluckPipe();
     expect(pipe).toBeTruthy();
   });
 
+  it('should puck values from a list of objects by the name of the property', () => {
+    // given
+    const pipe = new PluckPipe();
+    const objectList = [{id: '1', name: 'a'}, {id: '2', name: 'b'}];
 
+    // when
+    const pluckedByName = pipe.transform(objectList, 'name');
+
+    // then
+    expect(pluckedByName).toEqual(['a', 'b']);
+  });
 });
