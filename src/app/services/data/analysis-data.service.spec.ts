956f214e53cd0ef5e7b6e65bdd6d94116cf541d6
Henning Buhl <henning.buhl@yahoo.de>
2021-06-07 15:32:33 +0200

    Added loadAnalyses test case

--- src/app/services/data/analysis-data.service.spec.ts
+++ src/app/services/data/analysis-data.service.spec.ts
@@ -2,15 +2,19 @@ import {TestBed} from '@angular/core/testing';
 
 import {AnalysisDataService} from './analysis-data.service';
 import {SpecService} from '../spec.service';
+import {SampleDataService} from '../sample-data.service';
+import {Analysis} from '../../model/Analysis';
 
 describe('AnalysisDataService', () => {
   let service: AnalysisDataService;
+  let data: SampleDataService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: SpecService.imports,
     });
     service = TestBed.inject(AnalysisDataService);
+    data = TestBed.inject(SampleDataService);
   });
 
   it('should be created', () => {
@@ -20,10 +24,24 @@ describe('AnalysisDataService', () => {
   it('should initialize', () => {
     // given
 
-
     // when
 
     // then
 
+  });
+
+  it('should load analyses', () => {
+    // given
+    spyOn(service.loadedAnalyses, 'emit');
+
+    // when
+    service.loadedAnalyses.subscribe((analyses: Analysis[]) => {
+      expect(analyses).toEqual(data.analyses);
+    });
+    service.loadAnalyses();
+
+    // then
+    expect(service.loadedAnalyses.emit).toHaveBeenCalled();
+    expect(service.analyses).toEqual(data.analyses);
   });
 });
