ca06b263f72068e8c29d1436df3b2ac2536cdbb6
Henning Buhl <henning.buhl@yahoo.de>
2021-06-17 11:47:47 +0200

    Update log.service.ts

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -5,8 +5,8 @@ import {environment} from '../../environments/environment';
 Features:
 
 HUGE refactor: Rename + folder structure
-HTTP interceptor
-Show success/failure of http requests (possibility to repeat failed requests, and see log of requests)
+HTTP interceptor https://medium.com/angular-in-depth/top-10-ways-to-use-interceptors-in-angular-db450f8a62d6
+HTTP loader Show success/failure of http requests (possibility to repeat (or autorepeat) failed requests, and see log of requests)
 Localization (error codes in backend, change lang in frontend [localStorage]) https://stackblitz.com/github/ngx-translate/example?file=src%2Fapp%2Fapp.component.ts
 
 
