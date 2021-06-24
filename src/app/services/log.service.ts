2fe82be1a0e79416a5dbfb4aa8374e6375b6af80
Henning Buhl <henning.buhl@yahoo.de>
2021-06-24 21:51:13 +0200

    Update log.service.ts

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -34,7 +34,6 @@ Angular e2e tests
 Better 404 page (information about what might have been misinputted)
 Rework impact slider (arrow keys supported) and filtering (drag middle bar and change borders) and appear on tab, focus in requirement table, mat-slider has an ABSOLUTE min width...
 BUGGED: dialog analysis opens analysis when checkbox is not selected
-BUG: why is footer hidden in home component?
 Localization (Use values from jsons everywhere, translate them, change lang in frontend [localStorage] and overwrite the useDefaultOverBrowserLang option) [languageService, saves all available languages for selection, use it in selection and AppComponent]
 Impact slider in delta is completely bugged
 All Tables: How to deal with filter when adding new row? Remove filter? Display message if new row is hidden by filter? scroll to new row? Flash new row?
