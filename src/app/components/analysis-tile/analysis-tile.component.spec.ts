49dbf5fb371f6050938bfda5efa263f823ccb2ce
Henning Buhl <henning.buhl@yahoo.de>
2021-06-24 22:21:06 +0200

    Fixed test (passed Analysis)

--- src/app/components/analysis-tile/analysis-tile.component.spec.ts
+++ src/app/components/analysis-tile/analysis-tile.component.spec.ts
@@ -2,6 +2,8 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {AnalysisTileComponent} from './analysis-tile.component';
 import {SpecService} from '../../services/spec.service';
+import {MatDialogRef} from '@angular/material/dialog';
+import {Analysis} from '../../model/Analysis';
 
 describe('AnalysisTileComponent', () => {
   let component: AnalysisTileComponent;
@@ -11,14 +13,14 @@ describe('AnalysisTileComponent', () => {
     await TestBed.configureTestingModule({
       imports: SpecService.imports,
       providers: SpecService.providers,
-      declarations: [ AnalysisTileComponent ]
-    })
-    .compileComponents();
+      declarations: [AnalysisTileComponent]
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AnalysisTileComponent);
     component = fixture.componentInstance;
+    component.analysis = new Analysis();
     fixture.detectChanges();
   });
 
