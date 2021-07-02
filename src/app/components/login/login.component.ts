888e35bd1993b43c82639890d9f4b182e5c8c9b9
Henning Buhl <henning.buhl@yahoo.de>
2021-07-03 00:30:48 +0200

    Update login.component.ts

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -52,13 +52,20 @@ export class LoginComponent implements OnInit, AfterViewInit {
     }
 
     console.log('-----------------------------------------------');
-    console.log('-----------------------------------------------');
-    console.log('-----------------------------------------------');
-    console.log('-----------------------------------------------');
-    console.log('-----------------------------------------------');
     console.log(environment.authEnabled);
     console.log(environment.authMultiTenancyEnabled);
     console.log(environment.useDefaultOverBrowserLang);
+    console.log('-----------------------------------------------');
+    console.log(environment.authEnabled === true);
+    console.log(environment.authEnabled == true);
+    console.log(environment.authEnabled === 'true');
+    console.log(environment.authEnabled == 'true');
+    console.log('-----------------------------------------------');
+    console.log(environment.authEnabled === false);
+    console.log(environment.authEnabled == false);
+    console.log(environment.authEnabled === 'false');
+    console.log(environment.authEnabled == 'false');
+
   }
 
   ngAfterViewInit(): void {
