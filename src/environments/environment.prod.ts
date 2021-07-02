52e7a9794f82f7bcc2a589d9f5d53e57e44e9a0e
Henning Buhl <henning.buhl@yahoo.de>
2021-07-03 01:01:06 +0200

    hjk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ export const environment = {
   serverAddr: 'SERVER_ADDR',
   backendPort: 'BACKEND_PORT',
   defaultLang: 'DEFAULT_LANGUAGE',
-  useDefaultOverBrowserLang: 'USE_DEFAULT_OVER_BROWSER_LANGUAGE',
-  authEnabled: 'AUTH_ENABLED',
-  authMultiTenancyEnabled: 'AUTH_MULTI_TENANCY_ENABLED',
+  useDefaultOverBrowserLang: USE_DEFAULT_OVER_BROWSER_LANGUAGE,
+  authEnabled: AUTH_ENABLED,
+  authMultiTenancyEnabled: AUTH_MULTI_TENANCY_ENABLED,
   authPort: 'AUTH_PORT'
 };
