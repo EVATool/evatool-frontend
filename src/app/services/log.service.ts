76d1711ff25378bda45f62502c40b0e7fd879780
Henning Buhl <henning.buhl@yahoo.de>
2021-08-09 16:21:57 +0200

    Moved TODOs into Jira

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,64 +1,6 @@
 import {Injectable} from '@angular/core';
 import {environment} from '../../environments/environment';
 
-/*
-TODO:
-EvaTool Jira
-
-Sample Data (Import/Export) Use own json format for backend rest calls (import + export)
-Standalone version with user registration (create new realm, with user and password etc...)
-
-Delete requirement button in req table is in the middle?
-Stakeholder prio filter UI too many elements (not cleared?)
-Make UI work in firefix/Chrome
-keycloak docker container can auth from frontend, but access from backend to keycloak container always causes 401 in prod
-Tasks aus Jira (UID) [remove duplicates, ]
-
-better documentation in repos for how to start aux docker containers
-add mock-prod profile to front and backend (partial docker compose that starts all containers except backend and frontend)
-Documentation of docker-compose env vars
-More extensive useage of env vars (default realm, container names, etc...)
-git fetch causes permission error in deploy GitHub Actions
-Standalone flag for public version? Uses can register and have their own realm?
-Redirect to home from login component when auth is disabled?
-docker: rework networks (add auth network?) networks only required for inter-container communication
-Only purge data script (ONLY database with main data, DELETE FROM ANALYSIS)
-All Tables: How to deal with filter when adding new row? Remove filter? Display message if new row is hidden by filter? scroll to new row? Flash new row?
-How to deal with http status code 0? Ignore it? and hide the snackbar when the status is 0 (only in prod)?
-Filtering: And/Or, what is the default filtering? (how does this apply to deltas?)
-
-BACKLOG:
-Localization (Use values from jsons everywhere, translate them, change lang in frontend [localStorage] and overwrite the useDefaultOverBrowserLang option) [languageService, saves all available languages for selection, use it in selection and AppComponent]
-Catch 404 in login form when realm does not exist (why does 404 not appear in frontend but in postman?)
-keycloak is not configured properly yet (loging redirecting not working, /h2-console and /swagger are not reachable)
-keycloak login on non-existent realm causes CORS error, only in prod?) [snackBar at bottom, status 0]
-keycloak How to make default realm export file reuseable to create a new standard realm (or even n realms) with a set name
-keycloak Test changing users of default realm and restarting docker-compose. Does re-import overwrite changes?
-Better 404 page (information about what might have been misinputted)
-Log to file (frontend and backend container)
-Rework impact slider (arrow keys supported) and filtering (drag middle bar and change borders) and appear on tab, focus in requirement table, mat-slider has an ABSOLUTE min width...
-Cashe login credentials locally (at least refresh token) and load when page is refreshed
-
-BUGS:
-Visuals of ValueModal changes when there is no entry in impact table. Mat form required to load??
-Strange bug when negative impact was only slidable like a positive in requirements table
-Strange bug where requirement table contained requirements with same prefixSequenceId. How can this happen? Was that the fault of the updateFromDto method?
-BUGGED: dialog analysis opens analysis when checkbox is not selected
-
-MISC:
-Single user: reload data when tab changes? (how to update references?)
-Multi user: concurrency... (PWA?)
-Angular e2e tests
-Huge styling refactor (Zooming, mobile, resizing)
-SonarCloud Status not shown in backend
-make Sonarcloud Github Action work even if tests fail (front [] and backend [-Dmaven.test.failure.ignore=true])
-control click (or middle click) on analysis tile -> new tab
-Determine convention for html tag order and enforce (e.g. matInput, [input], (output), ([bind]))
-rework material forms (validation)
-docker container of mysql takes very long to start, this backend container fails (docker desktop on windows) [workaournd: wait 5 minutes for mysql database, then restart backend] (use postgres instead of mysql??)
-
-*/
-
 export enum LogLevel {
   Trace = 0,
   Debug = 1,
