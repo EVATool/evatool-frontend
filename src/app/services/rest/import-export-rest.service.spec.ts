7f15ed618e03d1d08ba3acc63d88266061384583
Henning Buhl <henning.buhl@yahoo.de>
2021-08-13 14:43:12 +0200

    Made test pass

--- src/app/services/rest/import-export-rest.service.spec.ts
+++ src/app/services/rest/import-export-rest.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ImportExportRestService } from './import-export-rest.service';
+import {SpecService} from '../spec.service';
 
 describe('ImportExportRestService', () => {
   let service: ImportExportRestService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: SpecService.imports
+    });
     service = TestBed.inject(ImportExportRestService);
   });
 
