3b42e99ba80cb972962170362de2021aab3e5217
Henning Buhl <henning.buhl@yahoo.de>
2021-08-24 11:35:53 +0200

    unsub in HttpLoaderComponent

--- src/app/components/http-loader/http-loader.component.ts
+++ src/app/components/http-loader/http-loader.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {LogService} from '../../services/log.service';
 import {HttpLoaderService} from '../../services/http-loader.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
@@ -7,13 +7,17 @@ import {CrossUiEventService} from '../../services/cross-ui-event.service';
 import {Router} from '@angular/router';
 import {ROUTES} from '../../app-routes';
 import {environment} from '../../../environments/environment';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-http-loader',
   templateUrl: './http-loader.component.html',
   styleUrls: ['./http-loader.component.scss']
 })
-export class HttpLoaderComponent implements OnInit {
+export class HttpLoaderComponent implements OnInit, OnDestroy {
+
+  private ngUnsubscribe = new Subject();
 
   loadingSpinnerShown = false;
   snackBarShown = false;
@@ -27,13 +31,13 @@ export class HttpLoaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.httpLoaderService.httpActive.subscribe(() => {
+    this.httpLoaderService.httpActive.pipe(takeUntil(this.ngUnsubscribe)).subscribe(() => {
       this.logger.debug(this, 'There are active http requests');
       this.successIconShown = false;
       this.loadingSpinnerShown = true;
     });
 
-    this.httpLoaderService.httpNotActive.subscribe((lastHttpEvent: HttpInfo) => {
+    this.httpLoaderService.httpNotActive.pipe(takeUntil(this.ngUnsubscribe)).subscribe((lastHttpEvent: HttpInfo) => {
       this.logger.debug(this, 'There are NO active http requests');
       this.loadingSpinnerShown = false;
 
@@ -46,18 +50,18 @@ export class HttpLoaderComponent implements OnInit {
       }
     });
 
-    this.crossUI.authenticationFailed.subscribe(() => { // 401
+    this.crossUI.authenticationFailed.pipe(takeUntil(this.ngUnsubscribe)).subscribe(() => { // 401
       if (!this.router.url.includes(ROUTES.realmAdministration)) {
         this.router.navigate([ROUTES.login]);
       }
     });
 
-    this.crossUI.authorizationFailed.subscribe(() => { // 403
+    this.crossUI.authorizationFailed.pipe(takeUntil(this.ngUnsubscribe)).subscribe(() => { // 403
       this.router.navigate([ROUTES.accessDenied]);
     });
 
     // Generic http error display.
-    this.httpLoaderService.httpError.subscribe((httpInfo: HttpInfo) => {
+    this.httpLoaderService.httpError.pipe(takeUntil(this.ngUnsubscribe)).subscribe((httpInfo: HttpInfo) => {
       if (!environment.production) {
         console.log(httpInfo); // This should stay here.
       }
@@ -71,10 +75,15 @@ export class HttpLoaderComponent implements OnInit {
         const message = 'An http request failed (status ' + httpInfo.httpStatusCode + ')';
         const action = '';
         const snackBarRef = this.snackBar.open(message, action, {duration: 5000});
-        snackBarRef.afterDismissed().subscribe(() => {
+        snackBarRef.afterDismissed().pipe(takeUntil(this.ngUnsubscribe)).subscribe(() => {
           this.snackBarShown = false;
         });
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
