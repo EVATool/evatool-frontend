c7ccf3479b41b3456983d6e976c666f486e38a5c
Henning Buhl <henning.buhl@yahoo.de>
2021-08-30 17:28:49 +0200

    Disabled csrf when auth is disabled

--- src/app/services/auth/xsrf-interceptor.service.ts
+++ src/app/services/auth/xsrf-interceptor.service.ts
@@ -1,29 +1,32 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpXsrfTokenExtractor} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {LogService} from '../log.service';
+import {environment} from '../../../environments/environment';
 
 
 @Injectable()
 export class XsrfInterceptor implements HttpInterceptor {
 
-  constructor(private tokenExtractor: HttpXsrfTokenExtractor) {
+  constructor(private tokenExtractor: HttpXsrfTokenExtractor,
+              private logger: LogService) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (environment.authEnabled) {
+      let requestMethod: string = req.method;
+      requestMethod = requestMethod.toLowerCase();
 
-    let requestMethod: string = req.method;
-    requestMethod = requestMethod.toLowerCase();
+      this.logger.info(this, 'XSRF-TOKEN: ' + this.tokenExtractor.getToken());
+      req = req.clone({withCredentials: true});
 
-    console.log(this.tokenExtractor.getToken());
-    req = req.clone({withCredentials: true});
-
-    if (requestMethod && (requestMethod === 'post' || requestMethod === 'delete' || requestMethod === 'put')) {
-      const headerName = 'X-XSRF-TOKEN';
-      const token = this.tokenExtractor.getToken() as string;
-      if (token !== null && !req.headers.has(headerName)) {
-        req = req.clone({headers: req.headers.set(headerName, token)});
+      if (requestMethod && (requestMethod === 'post' || requestMethod === 'delete' || requestMethod === 'put')) {
+        const headerName = 'X-XSRF-TOKEN';
+        const token = this.tokenExtractor.getToken() as string;
+        if (token !== null && !req.headers.has(headerName)) {
+          req = req.clone({headers: req.headers.set(headerName, token)});
+        }
       }
-    } else if (requestMethod && requestMethod === 'get') {
     }
 
     return next.handle(req);
