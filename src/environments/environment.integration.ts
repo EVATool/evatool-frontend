0dd97093ee2ad5ca840f12d8c9f84c7c3fc1efab
Henning Buhl <henning.buhl@yahoo.de>
2021-09-09 19:34:54 +0200

    Added integration profile and run config

---
+++ src/environments/environment.integration.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  testing: false,
+  serverProtocol: 'http',
+  serverAddr: 'localhost',
+  backendPort: '8080',
+  defaultLang: 'en',
+  useDefaultOverBrowserLang: true,
+  authEnabled: true,
+  authMultiTenancyEnabled: true,
+  authPort: '8081',
+  authRegistrationEnabled: true,
+  logLevel: 'INFO'
+};
