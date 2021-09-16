2489dd2c9d1dbefb4e22a6881271922b5aebf908
Henning Buhl <henning.buhl@yahoo.de>
2021-09-16 13:07:57 +0200

    Added formatpipe test

--- src/app/pipes/string-format.pipe.spec.ts
+++ src/app/pipes/string-format.pipe.spec.ts
@@ -1,8 +1,21 @@
-import { StringFormatPipe } from './string-format.pipe';
+import {StringFormatPipe} from './string-format.pipe';
 
 describe('StringFormatPipe', () => {
   it('create an instance', () => {
     const pipe = new StringFormatPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should puck values from a list of objects by the name of the property', () => {
+    // given
+    const pipe = new StringFormatPipe();
+    const formatString = 'A ${1} B ${0} C';
+    const args = ['X', 'y'];
+
+    // when
+    const formatted = pipe.transform(formatString, ...args);
+
+    // then
+    expect(formatted).toEqual('A y B X C');
   });
 });
