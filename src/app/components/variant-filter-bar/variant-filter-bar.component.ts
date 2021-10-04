7495753b7a760f16e254597c72a0e00ccca77326
Henning Buhl <henning.buhl@yahoo.de>
2021-10-04 18:58:46 +0200

    implemented variant filter bar

--- src/app/components/variant-filter-bar/variant-filter-bar.component.ts
+++ src/app/components/variant-filter-bar/variant-filter-bar.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Subject} from 'rxjs';
+import {FilterCategoryComponent} from '../filter-category/filter-category.component';
+import {LogService} from '../../services/log.service';
+import {takeUntil} from 'rxjs/operators';
+import {VariantTableFilterEvent} from './VariantTableFilterEvent';
+import {VariantTypeDataService} from '../../services/data/variant-type-data.service';
+import {VariantType} from '../../model/VariantType';
 
 @Component({
   selector: 'app-variant-filter-bar',
@@ -7,9 +14,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VariantFilterBarComponent implements OnInit {
 
-  constructor() { }
+  private ngUnsubscribe = new Subject();
 
-  ngOnInit(): void {
+  @ViewChild('variantTypeFilter') variantTypeFilter!: FilterCategoryComponent;
+  @Output() filterChanged = new EventEmitter<VariantTableFilterEvent>();
+
+  variantTypeNames!: string[];
+
+  variantTableFilterEvent: VariantTableFilterEvent;
+  suppressChildEvent = false;
+
+  constructor(private logger: LogService,
+              public variantTypeDataService: VariantTypeDataService) {
+    this.variantTableFilterEvent = VariantTableFilterEvent.getDefault();
   }
 
+  ngOnInit(): void {
+    this.variantTypeDataService.loadedVariantTypes
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((variantTypes: VariantType[]) => {
+        this.updateVariantTypes();
+      });
+
+    this.variantTypeDataService.createdVariantType
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((variantType: VariantType) => {
+        this.updateVariantTypes();
+      });
+
+    this.variantTypeDataService.updatedVariantType
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((variantType: VariantType) => {
+        this.updateVariantTypes();
+      });
+
+    this.variantTypeDataService.deletedVariantType
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((variantType: VariantType) => {
+        this.updateVariantTypes();
+      });
+
+    this.updateVariantTypes();
+  }
+
+  updateVariantTypes(): void {
+    this.variantTypeNames = this.variantTypeDataService.variantTypes.map(
+      variantType => variantType.name);
+  }
+
+  variantTypeFilterChanged(variantType: any): void {
+    this.variantTableFilterEvent.variantType = variantType;
+    if (!this.suppressChildEvent) {
+      this.filterChanged.emit(this.variantTableFilterEvent);
+    }
+  }
+
+  clearFilter(): void {
+    this.suppressChildEvent = true;
+
+    this.variantTypeFilter.clearFilter();
+
+    this.suppressChildEvent = false;
+    this.filterChanged.emit(this.variantTableFilterEvent);
+  }
 }
