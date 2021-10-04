597ba7398dd73a78ee04fe70b84ebaae5328f1b0
Henning Buhl <henning.buhl@yahoo.de>
2021-10-04 09:53:58 +0200

    Implemented ValueType and VariantType MapperServices

--- src/app/services/mapper/value-type-mapper.service.ts
+++ src/app/services/mapper/value-type-mapper.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {LogService} from '../log.service';
+import {ValueType} from '../../model/ValueType';
+import {ValueTypeDto} from '../../dto/ValueTypeDto';
+import {Analysis} from '../../model/Analysis';
+import {MapperService} from './mapper.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ValueTypeMapperService {
+export class ValueTypeMapperService extends MapperService {
 
-  constructor() { }
+  constructor(protected  logger: LogService) {
+    super(logger);
+  }
+
+  toDto(valueType: ValueType): ValueTypeDto {
+    this.logger.debug(this, 'Mapping ValueType to ValueTypeDto');
+    const valueTypeDto = new ValueTypeDto();
+
+    valueTypeDto.id = valueType.id;
+    valueTypeDto.name = valueType.name;
+    valueTypeDto.description = valueType.description;
+
+    return valueTypeDto;
+  }
+
+  fromDto(valueTypeDto: ValueTypeDto, analyses: Analysis[]): ValueType {
+    this.logger.debug(this, 'Mapping ValueTypeDto to ValueType');
+    const valueType = new ValueType();
+    this.updateFromDto(valueTypeDto, valueType, analyses);
+    return valueType;
+  }
+
+  updateFromDto(valueTypeDto: ValueTypeDto, valueType: ValueType, analyses: Analysis[]): void {
+    valueType.id = valueTypeDto.id;
+    valueType.name = valueTypeDto.name;
+    valueType.description = valueTypeDto.description;
+    for (const analysis of analyses) {
+      if (analysis.id === valueTypeDto.analysisId) {
+        valueType.analysis = analysis;
+        break;
+      }
+    }
+  }
 }
--- src/app/services/mapper/variant-type-mapper.service.ts
+++ src/app/services/mapper/variant-type-mapper.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {MapperService} from './mapper.service';
+import {LogService} from '../log.service';
+import {VariantType} from '../../model/VariantType';
+import {VariantTypeDto} from '../../dto/VariantTypeDto';
+import {Analysis} from '../../model/Analysis';
 
 @Injectable({
   providedIn: 'root'
 })
-export class VariantTypeMapperService {
+export class VariantTypeMapperService extends MapperService {
 
-  constructor() { }
+  constructor(protected  logger: LogService) {
+    super(logger);
+  }
+
+  toDto(variantType: VariantType): VariantTypeDto {
+    this.logger.debug(this, 'Mapping VariantType to VariantTypeDto');
+    const variantTypeDto = new VariantTypeDto();
+
+    variantTypeDto.id = variantType.id;
+    variantTypeDto.name = variantType.name;
+    variantTypeDto.description = variantType.description;
+
+    return variantTypeDto;
+  }
+
+  fromDto(variantTypeDto: VariantTypeDto, analyses: Analysis[]): VariantType {
+    this.logger.debug(this, 'Mapping VariantTypeDto to VariantType');
+    const variantType = new VariantType();
+    this.updateFromDto(variantTypeDto, variantType, analyses);
+    return variantType;
+  }
+
+  updateFromDto(variantTypeDto: VariantTypeDto, variantType: VariantType, analyses: Analysis[]): void {
+    variantType.id = variantTypeDto.id;
+    variantType.name = variantTypeDto.name;
+    variantType.description = variantTypeDto.description;
+    for (const analysis of analyses) {
+      if (analysis.id === variantTypeDto.analysisId) {
+        variantType.analysis = analysis;
+        break;
+      }
+    }
+  }
 }
