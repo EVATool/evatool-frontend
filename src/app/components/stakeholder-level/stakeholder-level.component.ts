f449d61eaf6a6ee1c19df546025691da89dd5edf
Henning Buhl <henning.buhl@yahoo.de>
2021-10-11 17:17:22 +0200

    Added stakeholder  property transltions

--- src/app/components/stakeholder-level/stakeholder-level.component.ts
+++ src/app/components/stakeholder-level/stakeholder-level.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {LogService} from '../../services/log.service';
 import {StakeholderDataService} from '../../services/data/stakeholder-data.service';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-stakeholder-level',
@@ -16,13 +17,25 @@ export class StakeholderLevelComponent {
   @Output() levelChange = new EventEmitter<string | null>();
 
   public shownLevels = [
-    {key: 'INDIVIDUAL', value: 'Individuell'},
-    {key: 'ORGANIZATION', value: 'Organisation'},
-    {key: 'SOCIETY', value: 'Gesellschaft'}
+    {key: 'INDIVIDUAL', value: 'Individual'},
+    {key: 'ORGANIZATION', value: 'Organization'},
+    {key: 'SOCIETY', value: 'Society'}
   ];
 
   constructor(private logger: LogService,
-              public stakeholderData: StakeholderDataService) {
+              public stakeholderData: StakeholderDataService,
+              private translate: TranslateService) {
+    this.translate.get('COMMON.PROPERTY.STAKEHOLDER.LEVEL_INDIVIDUAL', {value: 'world'}).subscribe((res: string) => {
+      this.shownLevels[0].value = res;
+    });
+
+    this.translate.get('COMMON.PROPERTY.STAKEHOLDER.LEVEL_ORGANIZATION', {value: 'world'}).subscribe((res: string) => {
+      this.shownLevels[1].value = res;
+    });
+
+    this.translate.get('COMMON.PROPERTY.STAKEHOLDER.LEVEL_SOCIETY', {value: 'world'}).subscribe((res: string) => {
+      this.shownLevels[2].value = res;
+    });
   }
 
   onLevelChange(): void {
